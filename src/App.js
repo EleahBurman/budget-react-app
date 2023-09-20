591c2d13de8e7d062826d44892f4d4c51234466c
eleahburman <eleahjburman@gmail.com>
2023-09-19 21:42:44 -0400

    Create utils and first card

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Button, Stack } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
+import BudgetCard from './components/BudgetCard';
 import './App.css';
 
 function App() {
@@ -9,7 +10,16 @@ function App() {
       <Button variant="primary">Add Budget</Button>
       <Button variant="outline-primary">Add Expense</Button>
     </Stack>
-    <div style={{ display: "grid", gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))", gap: "1rem", alignItems: "flex-start"}}></div>
+    <div style={{ 
+      display: "grid", 
+      gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))", 
+      gap: "1rem", 
+      alignItems: "flex-start"}}
+    >
+      <BudgetCard name="Entertainment" amount={200} max={1000}>
+
+      </BudgetCard>
+    </div>
   </Container>
 }
 
