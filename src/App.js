054869e272dfc058fc9de3fa8a1ef9f0d306584d
eleahburman <eleahjburman@gmail.com>
2023-09-19 21:31:00 -0400

    Add css properties to main app for spacing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import { Container, Stack } from 'react-bootstrap';
+import { Button, Stack } from 'react-bootstrap';
+import Container from 'react-bootstrap/Container';
 import './App.css';
 
 function App() {
-  return <Container>
+  return <Container className="my-4">
     <Stack direction="horizontal" gap="2" className="mb-4">
-      <ha className="me-auto"></ha>
+      <h1 className="me-auto">Budgets</h1>
+      <Button variant="primary">Add Budget</Button>
+      <Button variant="outline-primary">Add Expense</Button>
     </Stack>
+    <div style={{ display: "grid", gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))", gap: "1rem", alignItems: "flex-start"}}></div>
   </Container>
 }
 
